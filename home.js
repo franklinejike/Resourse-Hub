58630f4f23e3f44aeb068ef2097dfa7c5809e153
Franklin <franklinnwekpa@gmail.com>
2025-02-13 13:07:54 +0100

    DELETING OF OLD FILES

--- home.js
+++
@@ -1,9 +0,0 @@
-function displayNav(){
-    let mobileNav = document.querySelector(".mobileNav")
-    mobileNav.style.display= "block"
-}
-
-function removeNav(){
-    let mobileNav = document.querySelector(".mobileNav")
-    mobileNav.style.display= "none"
-}
